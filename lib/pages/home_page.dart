633edc51bf36448c56d2aeb1c98f2b22b2f4a93f
dapraws <darrelprawira26@gmail.com>
2024-01-25 19:15:05 +0700

    home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:motionintern_week2/themes/colors.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Text("home"),
-      ),
-    );
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: black,
+          centerTitle: true,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Image.asset("assets/icons/camera-icon.png"),
+          ),
+          title: Container(
+            width: 105,
+            child: Image.asset("assets/images/instagram-logo.png"),
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Image.asset("assets/icons/tv-icon.png"),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Image.asset("assets/icons/paperplane-icon.png"),
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                child: Image.asset("assets/images/home-stories.png"),
+              ),
+              Container(
+                child: Image.asset("assets/images/home-posts.png"),
+              ),
+            ],
+          ),
+        ));
   }
 }
