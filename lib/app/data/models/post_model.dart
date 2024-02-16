8a568757e200feaf49076c025bd2012da77b451d
dapraws <darrelprawira26@gmail.com>
2024-02-16 18:02:08 +0700

    add post model

--- lib/app/data/models/post_model.dart
+++ lib/app/data/models/post_model.dart
@@ -1,1 +1,18 @@
-class PostModel {}
+class PostModel {
+  String fotoProfile;
+  String namaAkun;
+  bool isSponsor;
+  String postingGambar;
+  bool isLike;
+  int jumlahLike;
+  String description;
+  PostModel({
+    required this.fotoProfile,
+    required this.namaAkun,
+    this.isSponsor = false,
+    required this.postingGambar,
+    this.isLike = false,
+    required this.jumlahLike,
+    required this.description,
+  });
+}
