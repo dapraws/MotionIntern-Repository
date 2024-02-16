18be864220d5e1b4e457c33332394a1a883adb34
dapraws <darrelprawira26@gmail.com>
2024-02-16 23:47:53 +0700

    add top appbar

---
+++ lib/app/shared/widgets/top_appbar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class TopAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final double height;
+
+  const TopAppBar({Key? key, this.height = kToolbarHeight}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Image.asset(
+                'assets/images/instagram-logo.png',
+                height: 40,
+              ),
+              IconButton(
+                icon: Icon(Icons.arrow_drop_down_outlined),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.favorite_outline_outlined),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.messenger_outline_outlined),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.add_box_outlined),
+                onPressed: () {},
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(height);
+}
