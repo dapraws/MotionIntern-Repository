de0cd9fe17a56e913a90081e23d5495c7ed3a75c
dapraws <darrelprawira26@gmail.com>
2024-02-16 22:29:57 +0700

    add app routes

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -4,9 +4,19 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
+  static const IG_STORY = _Paths.IG_STORY;
+  static const EXPLORE = _Paths.EXPLORE;
+  static const ADD_POST = _Paths.ADD_POST;
+  static const REELS = _Paths.REELS;
+  static const PROFILE = _Paths.PROFILE;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
+  static const IG_STORY = '/ig-story';
+  static const EXPLORE = '/explore';
+  static const REELS = '/reels';
+  static const ADD_POST = '/add-post';
+  static const PROFILE = '/profile';
 }
